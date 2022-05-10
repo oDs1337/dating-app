c69f5959b5696fe01e42278039c121eabef78c62
oDs <odstobiaszcod@gmail.com>
2022-05-10 11:56:28 +0200

    generated login form component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { LoginFormComponent } from './login-form/login-form.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
